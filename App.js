e0b2eb78906db05fbf2df66cf62b8424a520496f
loloa555 <38428597+loloa555@users.noreply.github.com>
2023-10-16 12:40:16 +1100

    Add files via upload

--- App.js
+++ App.js
@@ -1,19 +1,27 @@
 import React from 'react';
 import {Text, View} from 'react-native';
+ 
+  var cat = {
+        name: "Amy",
+        age: 2,
+        gender: "male",
+        address: "epping"
+      };
+
 
 const Cat = (name) => {
-  return 'hello, i am '+ name;
+  return 'hello, i am '+ name +' in cat';
 };
 {/* {name} argument must be used within <>tags*/}
 const Cats =(name) =>{
  return(
-   <Text>hello, cats of {name}</Text>
+   <Text>hello, cats of {name.n1}</Text>
  );
 };
 const Cat2 = props => {
   return (
     <View>
-      <Text>Hello, I am {props.name}!</Text>
+      <Text>Hello, I am {props.name1} in Cat2!</Text>
     </View>
   );
 };
@@ -22,11 +30,14 @@ const Cafe = (name2) => {
   return (
     <View>
      <Text> {Cat("Tom")} </Text>
-     <Text> {Cats("Lol")} </Text>
+     <Text> {Cats(n1="Lol")} </Text>
      <Text> hello, {name2} cafe</Text>
-     {/* <Cat2> using props call the Cat2 function is differnt with pass value into const Cats =(name) =>{};, and keep the attribute name of props the same props.name could be props.name1 with <Cat2 name1='Maru'> */}
-     <Cat2 name='Maru' />     
-     <Cat name="Maru" /> 
+     {/* <Cat2> using props call the Cat2 function is differnt with pass value into const Cats =(name) =>{};, and keep the attribute name of props the same as props.name, eg. props.name1 with <Cat2 name1='Maru'> */}
+     <Cat2 name1='Maru'></Cat2>     
+     <Cat name1="Maru" /> 
+     <Cats n1='Maru' /> 
+
+     <Cat2 name1={cat.name} > </Cat2>
     </View>
   );
 };
